6798980c4f1fa5f3d68a34d324a7ef8394ab04d1
Styopa Semenukha <semenukha@gmail.com>
2018-10-28 15:35:09 -0400

    Created boilerplate

---
+++ isempty.c
@@ -0,0 +1,6 @@
+/*
+Copyright 2018 Styopa Semenukha
+This file is part of isempty, see COPYING
+*/
+
+#include "isempty.h"
---
+++ isempty.h
@@ -0,0 +1,9 @@
+/*
+Copyright 2018 Styopa Semenukha
+This file is part of isempty, see COPYING
+*/
+
+#ifndef _ISEMPTY_H
+#define _ISEMPTY_H
+
+#endif // _ISEMPTY_H
