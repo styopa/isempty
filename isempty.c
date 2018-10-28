cb5a97379e9ac31550e28912b53dcb9c0efeef19
Styopa Semenukha <semenukha@gmail.com>
2018-10-28 16:56:37 -0400

    Parse options

--- isempty.c
+++ isempty.c
@@ -3,7 +3,36 @@ Copyright 2018 Styopa Semenukha
 This file is part of isempty, see COPYING
 */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "isempty.h"
 
-int main(const int argc, const char *argv[]) {
+int main(const int argc, char * const argv[])
+{
+    int flags, opt;
+    unsigned char byte = 0;
+    const char *file_name;
+
+    while ((opt = getopt(argc, argv, "b:")) != -1) {
+        switch (opt) {
+        case 'b':
+            byte = (unsigned char) atoi(optarg);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-b 0] FILE_NAME\n", argv[0]);
+            exit(ERROR_OPTIONS);
+        }
+    }
+
+    printf("byte = %hhi\n", byte);
+
+    if (optind >= argc) {
+        fprintf(stderr, "File name required\n");
+        exit(ERROR_OPTIONS);
+    }
+
+    file_name = argv[optind];
+    printf("File name: %s\n", file_name);
 }
--- isempty.h
+++ isempty.h
@@ -6,4 +6,6 @@ This file is part of isempty, see COPYING
 #ifndef _ISEMPTY_H
 #define _ISEMPTY_H
 
+#define ERROR_OPTIONS 2
+
 #endif // _ISEMPTY_H
